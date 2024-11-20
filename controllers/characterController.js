62eaabcaa5defdfc87390800253bcf063a12af62
Ember Doyle <doyle.ember@gmail.com>
2024-11-20 08:45:19 -0700

    Change number types

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -3,6 +3,7 @@ const db = require("../prisma/queries.js");
 async function allCharactersGet (req, res) {
     const coords = await db.findAllCharacters();
     console.log(coords)
+    console.log(typeof coords[0].x)
     res.json(coords);
     
 }
