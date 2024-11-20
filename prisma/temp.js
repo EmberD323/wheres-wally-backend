409830d422ab4ffe183b4bbb842bf0d3447524b3
Ember Doyle <doyle.ember@gmail.com>
2024-11-20 08:52:49 -0700

    Change number types

--- prisma/temp.js
+++ prisma/temp.js
@@ -7,15 +7,15 @@ async function main() {
   })
   const coord1 = await prisma.coords.create({
     data: {
-      x:  new Prisma.Decimal(47.2845),
-      y: new Prisma.Decimal(33.6794),
+      x:  '47.2845',
+      y: '33.6794',
       character: 'Wally'
     },
   })
   const coord2 = await prisma.coords.create({
     data: {
-      x: 69.86,
-      y: 37.9008,
+      x: '69.86',
+      y: '37.9008',
       character: 'Wenda'
     }
   })
