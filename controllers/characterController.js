4bbb3941e084c0de7647e3404413f418081f9df2
Ember Doyle <doyle.ember@gmail.com>
2024-11-20 09:18:07 -0700

    data

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -3,6 +3,7 @@ const db = require("../prisma/queries.js");
 async function allCharactersGet (req, res) {
     const coords = await db.findAllCharacters();
     console.log(coords)
+    console.log(coords[0].x)
     console.log(typeof coords[0].x)
     res.json(coords);
     
@@ -13,5 +14,4 @@ async function allCharactersGet (req, res) {
 
 module.exports = {
     allCharactersGet,
-    characterCheck
 };
