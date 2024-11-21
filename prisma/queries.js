974f1c7213ed7b5ef27d11c23ee2b63a94b44d7f
Ember Doyle <doyle.ember@gmail.com>
2024-11-21 11:16:59 -0700

    Order scores asc

--- prisma/queries.js
+++ prisma/queries.js
@@ -22,7 +22,7 @@ async function findAllScores() {
     const all =await prisma.scores.findMany({
         orderBy: [
           {
-            seconds:'desc',
+            seconds:'asc',
           },
         ],
       })
